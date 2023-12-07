765af85e2f8f2ddd1f81de71e5f40d450319d97c
Nathan Chevalier <cerberusgfx@gmail.com>
2023-12-07 15:58:13 -0600

    WIP

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,4 +1,38 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const Create = () => {
-    return <>CREATE IT</>;
-  };
-  
+  const navigate = useNavigate();
+  //! TODO(DRY): For loop to generate pattern index, pattern index = i 
+  const [patternChoices, setPatternChoices] = useState([
+    {
+      pattern_id: null,
+      pattern_index: 1,
+    },
+    {
+      pattern_id: null,
+      pattern_index: 2,
+    },
+    {
+      pattern_id: null,
+      pattern_index: 3,
+    },
+    {
+      pattern_id: null,
+      pattern_index: 4,
+    },
+    {
+      pattern_id: null,
+      pattern_index: 5,
+    },
+    {
+      pattern_id: null,
+      pattern_index: 6,
+    },
+  ]);
+  const [patterns, setPatterns] = useState([{
+    
+  }])
+
+  return <>CREATE IT</>;
+};
