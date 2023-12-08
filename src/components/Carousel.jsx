25bcb1c5d200146d4ce64c86814c45cb34bf4bc9
Nathan Chevalier <cerberusgfx@gmail.com>
2023-12-08 16:13:02 -0600

    Carousel prototype built

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export const Carousel = ({ cats }) => {
+  const [index, setIndex] = useState(0);
+  const length = cats.length;
+
+  const handlePrevious = () => {
+    const newIndex = index - 1;
+    setIndex(newIndex < 0 ? length - 1 : newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = index + 1;
+    setIndex(newIndex >= length ? 0 : newIndex);
+  };
+
+  return (
+    <div className="w-[60%] m-auto flex overflow-hidden">
+      <button className="btn-edit" onClick={handlePrevious}>
+        Previous
+      </button>
+      {cats.map((cat, i) => {
+        return (
+          <div
+            key={cat.id}
+            className={`${
+              i === index ? "flex flex-col items-center" : "hidden"
+            }`}
+          >
+            <span>{cat.label}</span>
+            <img src={`${cat.url}`} />
+          </div>
+        );
+      })}
+
+      <button className="btn-edit" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
