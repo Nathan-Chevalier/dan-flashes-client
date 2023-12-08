04e282c481406d871dccca2e1dc281b9769f2d71
Nathan Chevalier <cerberusgfx@gmail.com>
2023-12-08 09:32:51 -0600

    Input field added

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -4,33 +4,21 @@ import { getAllColors, getAllPatterns } from "../services/fetchServices";
 
 export const Create = () => {
   const navigate = useNavigate();
-  //! TODO(DRY): For loop to generate pattern index, pattern index = i
+  const [shirt, setShirt] = useState({
+    color: 1,
+    label: "useStateShirt",
+    public: false,
+    price: "",
+    patterns: []
+  })
+
   const [patternChoices, setPatternChoices] = useState([
     {
       pattern_id: null,
-      pattern_index: 1,
-    },
-    {
-      pattern_id: null,
-      pattern_index: 2,
-    },
-    {
-      pattern_id: null,
-      pattern_index: 3,
-    },
-    {
-      pattern_id: null,
-      pattern_index: 4,
-    },
-    {
-      pattern_id: null,
-      pattern_index: 5,
-    },
-    {
-      pattern_id: null,
-      pattern_index: 6,
-    },
+      pattern_index: null,
+    }
   ]);
+
   const [patterns, setPatterns] = useState([
     {
       id: null,
@@ -39,6 +27,7 @@ export const Create = () => {
       label: "NETWORK INACTIVE",
     },
   ]);
+
   const [colors, setColors] = useState([
     {
       id: null,
@@ -46,6 +35,8 @@ export const Create = () => {
       label: "NETWORK INACTIVE",
     },
   ]);
+
+  const basePrice = 60
 
   useEffect(() => {
     getAllColors().then((colorsArray) => {
@@ -59,7 +50,15 @@ export const Create = () => {
   return (
     <div className="__shirt-form-container__ flex flex-col items-center">
       <form className="__shirt-form__ h-[794px] w-[1278px] bg-orange-600 rounded-xl">
-        <div className="__label-preview-color-container__ flex flex-col"></div>
+        <div className="__label-preview-color-container__ flex flex-col">
+          <fieldset>
+            <input type="text" className="input-text" value={shirt.label} onChange={(event) => {
+              const copy = { ...shirt }
+              copy.label = event.target.value
+              setShirt(copy)
+            }}/>
+          </fieldset>
+        </div>
         <div className="__pattern-public-save-container__ flex flex-col"></div>
       </form>
     </div>
