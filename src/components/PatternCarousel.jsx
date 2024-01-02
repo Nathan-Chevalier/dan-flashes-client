e54976f8ab2054769f1a6dce50423f531a4c6831
Nathan Chevalier <cerberusgfx@gmail.com>
2024-01-02 16:13:20 -0600

    CSS classes built for carousel nav interaction

--- src/components/PatternCarousel.jsx
+++ src/components/PatternCarousel.jsx
@@ -53,13 +53,13 @@ export const PatternCarousel = ({
               X
             </button>
             <span className="bg-orange-600 px-4 py-[2px] w-max rounded-full text-white outline outline-white outline-2 z-10">{pattern.label}</span>
-            <div className="__pattern-preview)__ w-[170px] h-[170px] flex flex-col rounded-full overflow-hidden" style={{backgroundColor: `${shirtColor}`}}>
+            <div className="__pattern-preview)__ w-[170px] h-[170px] flex flex-col rounded-full overflow-hidden outline outline-white outline-4" style={{backgroundColor: `${shirtColor}`}}>
               <img src={pattern.pattern_preview} />
             </div>
 
-            <div className="__buttons-container__ flex items-center justify-between w-full mb-2">
+            <div className="__buttons-container__ flex items-center justify-between w-full mb-2 bg-orange-700 mt-1 px-2 py-[2px] rounded outline outline-white outline-4">
               <button
-                className="btn-edit h-[60px] w-[48px]"
+                className="__button-previous__ h-[60px] w-[48px]"
                 onClick={handlePrevious}
               >
                 P
