0ca4953cfdb9e90ca45022223bd49686487442c1
Nathan Chevalier <cerberusgfx@gmail.com>
2024-01-06 12:09:28 -0600

    Login page complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import flashesLogo from "../assets/flashes_logo_512.png";
 
 export const Login = () => {
@@ -96,15 +96,16 @@ export const Login = () => {
         </fieldset>
       </form>
 
-      <div className="loginLinks">
-        <section className="link--register">
-          <Link
-            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+      <div className="bg-cyan-500 mt-5 flex flex-col items-center w-[350px] py-4 rounded-3xl outline outline-white">
+      <span className="font-lobster text-white text-3xl bg-white/20 px-6 rounded-xl">Not a member yet?</span>
+          <NavLink
+            className="flex flex-col bg-orange-500 hover:bg-cyan-300 hover:text-orange-500 text-white font-bold py-1 px-2 border-white border-4 hover:border-white font-paytone text-md items-center justify-center cursor-pointer hover:outline-offset-2 hover:outline hover:outline-white h-20 w-20 rounded-full mt-4"
             to="/register"
           >
-            Not a member yet?
-          </Link>
-        </section>
+            <span className="translate-y-[5px]">BECOME</span>
+            <span className="-translate-y-[5px]">ONE</span>
+          </NavLink>
+
       </div>
     </main>
   );
